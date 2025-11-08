a35f5a1ec2c14f767ca066b07727fe094ba31afc
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2025-11-08 22:43:10 +0500

    update

---
+++ index.py
@@ -0,0 +1,11 @@
+# iEEE 754 floating point precision issue demonstration
+# float values
+if (Amir == 'Present'):
+    print("yes Amir is Present")
+    else:
+        print("Amir is not present")
+  # elif (John == 'yes John is present')
+  #   print("yes John is present")
+  # elif (Sara == 'Present'):
+  #   print("All students are not present") 
+  
