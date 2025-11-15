55f49a859f7c965845c32fe649c0620727138bf9
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2025-11-15 22:11:59 +0500

    update

--- expressionExecution.py
+++ expressionExecution.py
@@ -1,5 +1,4 @@
 # String & Numeric Values can operate together with *
-
 A, B = 2,3
 Txt = "a"
 result = 2*Txt *3
@@ -7,7 +6,12 @@ print(f"{result} Number of 'a' printed in : {len(result)} times")
 
 
 # String & Numeric can operate with = + (Concatenate).
-
 A, B = "2", 3
 txt = "s"
-print(A + txt * B)  
+print((A+txt)*B)  # precedence(A+txt) then *B
+
+
+# Numeric Value can operate with all arithmetic operators.
+A, B = 2, 3
+c = 4
+print(A + B* c)   # precedence(B*c) then +A
