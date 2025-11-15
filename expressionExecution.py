acd8f180be0ac9e183c90e95889b2fef153e3aca
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2025-11-15 21:54:41 +0500

    expression_execution

---
+++ expressionExecution.py
@@ -0,0 +1,6 @@
+# String & Numeric Values can operate together with *
+
+A, B = 2,3
+Txt = "a"
+result = 2*Txt *3
+print(f"{result} Number of 'a' printed in : {len(result)} times")  # Output: aaaaaa
