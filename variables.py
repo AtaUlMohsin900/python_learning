35d4119a8ded26fc6f8c9acb4477015218de3ada
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-12-09 18:56:18 +0000

    update

--- variables.py
+++ variables.py
@@ -13,3 +13,27 @@ person_info = {
    'country':'Pakistan',
    'city':'Islamabad'
    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Ataul', 'Mohsin', 'Pakistan', 45, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
