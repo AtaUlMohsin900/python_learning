35f42c3e9c23415c0b680ad2a6d3bcfc46a2d539
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-12-09 18:53:11 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Variables in Python
+
+first_name = 'Ataul'
+last_name = 'Mohsin'
+country = 'Pakistan'
+city = 'Islamabad'
+age = 45
+is_married = True
+skills = ['python', 'cpp', 'JS', 'Java', 'Ruby']
+person_info = {
+   'firstname':'Ataul',
+   'lastname':'Mohsin',
+   'country':'Pakistan',
+   'city':'Islamabad'
+   }
